75a74f0de8966677e93c7713e8605ffadf9f93bf
Olvany <hell_1914@hotmail.com>
2021-08-18 01:22:11 -0500

    commit Inicial

---
+++ src/main/java/utp/misiontic2022/c2/p51/reto4/modelo/vo/Requerimiento_1.java
@@ -0,0 +1,5 @@
+package utp.misiontic2022.c2.p51.reto4.modelo.vo;
+
+public class Requerimiento_1 {
+    // Su código
+}
---
+++ src/main/java/utp/misiontic2022/c2/p51/reto4/modelo/vo/Requerimiento_3.java
@@ -0,0 +1,5 @@
+package utp.misiontic2022.c2.p51.reto4.modelo.vo;
+
+public class Requerimiento_3 {
+    // Su código
+}
